465523dc2be695bf3847181b4a6e1b3453ca7d59
Sandeep Sharma <sandy335582@gmail.com>
2022-06-26 11:09:15 +0530

    bundling setup with hardcoded file content

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as esbuild from 'esbuild-wasm'
+import { useState,useEffect } from 'react';
 import './App.css';
+import { unpkgPathPlugin } from './components/plugin';
 
 function App() {
+  const [code, setCode] = useState("123");
+  const [inputValue, setInputValue] = useState("");
+  const submitData = async ()=>{
+    const transformedCode = await esbuild.build({
+      entryPoints:['index.js'],
+      bundle:true,
+      write:false,
+      plugins:[unpkgPathPlugin()]  
+    });
+    console.log(transformedCode)
+
+  }
+
+  useEffect(()=>{
+    startService();
+  },[]);
+
+  const startService = async ()=>{
+    await esbuild.initialize({
+      wasmURL:'/esbuild.wasm'
+    });
+    console.log("service", esbuild)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App m-1">
+      <div>
+        <textarea
+          value={inputValue}
+          cols={30}
+          rows={10}
+          onChange={(event)=>setInputValue(event?.target.value)}>
+        </textarea>
+      </div>
+      <button className='btn btn-primary m-1' onClick={submitData}> Submit </button>
+      <pre>{code}</pre>
     </div>
   );
 }
